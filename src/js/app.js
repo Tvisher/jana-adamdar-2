cb25a2b8e53dc85c534e018aff47af0125c02d81
Tvisher <tvisher@mail.ru>
2023-01-11 16:15:52 +0600

    add some adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -119,4 +119,8 @@ const waySlider = new Swiper('.way-slider', {
     slidesPerView: 'auto',
     mousewheel: true,
     spaceBetween: 40,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
