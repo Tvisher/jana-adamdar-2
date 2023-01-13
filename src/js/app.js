8e2b4363657598b95f55a3f945b36cc785937452
Tvisher <tvisher@mail.ru>
2023-01-13 10:17:26 +0600

    add aos

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 import * as baseFunction from './modules/functions.js';
 import './vendors/vendors.js';
-// import AOS from 'aos';
+import AOS from 'aos';
 import IMask from 'imask';
 import Rellax from 'rellax';
 
@@ -16,7 +16,28 @@ window.addEventListener('load', () => {
     setTimeout(() => {
         document.body.classList.add('load');
     }, 230);
-})
+});
+
+// Инит и опции библиотеки анимаций
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'load', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 25, // offset (in px) from the original trigger point
+    delay: 100, // values from 0 to 3000, with step 50ms
+    duration: 1200, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
 
 
 
@@ -146,3 +167,6 @@ const waySlider = new Swiper('.way-slider', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+
+
